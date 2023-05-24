dc9e8081908c7efcd71b74b8725add5382679342
Felipe Ribeiro <ribeiro.feu@gmail.com>
2023-05-24 20:11:36 -0300

    Commit inicial

---
+++ src/main/java/com/fiap/techfood/TechfoodApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.techfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/techfood/TechfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
